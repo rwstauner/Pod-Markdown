0d3596dde00b77ac052700c655b22bfa506aadc5
Randy Stauner <randy@magnificent-tears.com>
2012-11-17 08:27:00 -0700

    Move duplicate escape/interpolate logic to a method
    The only remaining call to _escape() (rather than _escape_non_code())
    was in the heading which Peter Vereshagin just fixed
    by copying the logic from textblock().
    
    So make it a method to slim down the two places using it.

--- lib/Pod/Markdown.pm
+++ lib/Pod/Markdown.pm
@@ -111,12 +111,7 @@ sub command {
     if ($command =~ m{head(\d)}xms) {
         my $level = $1;
 
-        # escape markdown characters in text sequences except for inline code
-        $paragraph = join '', $parser->parse_text(
-            { -expand_text => '_escape_non_code' },
-            $paragraph, $line_num
-        )->raw_text;
-        $paragraph = $parser->interpolate($paragraph, $line_num);
+        $paragraph = $parser->_escape_and_interpolate($paragraph, $line_num);
 
         # the headers never are indented
         $parser->_save($parser->format_header($level, $paragraph));
@@ -201,6 +196,21 @@ sub verbatim {
     $parser->_save($paragraph);
 }
 
+sub _escape_and_interpolate {
+    my ($parser, $paragraph, $line_num) = @_;
+
+    # escape markdown characters in text sequences except for inline code
+    $paragraph = join '', $parser->parse_text(
+        { -expand_text => '_escape_non_code' },
+        $paragraph, $line_num
+    )->raw_text;
+
+    # interpolate the paragraph for embedded sequences
+    $paragraph = $parser->interpolate($paragraph, $line_num);
+
+    return $paragraph;
+}
+
 sub _escape_non_code {
     my ($parser, $text, $ptree) = @_;
     $text = $parser->_escape($text)
@@ -213,14 +223,7 @@ sub textblock {
     my $data = $parser->_private;
     my $prelisthead;
 
-    # escape markdown characters in text sequences except for inline code
-    $paragraph = join '', $parser->parse_text(
-        { -expand_text => '_escape_non_code' },
-        $paragraph, $line_num
-    )->raw_text;
-
-    # interpolate the paragraph for embedded sequences
-    $paragraph = $parser->interpolate($paragraph, $line_num);
+    $paragraph = $parser->_escape_and_interpolate($paragraph, $line_num);
 
     # clean the empty lines
     $paragraph = $parser->_clean_text($paragraph);
